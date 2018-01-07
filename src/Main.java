57d1e673b467380438cea8178e54f186cbc26f60
posusaevkostay@mail.ru <posusaevkostay@mail.ru>
2018-01-07 21:21:54 +0300

    Task39

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        int arr[] = new int[4];
+
+        for (int i = 0; i < arr.length; i++) {
+            int a = (int) (Math.random() * 89) + 10;
+            arr[i] = a;
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+
+        if (arr[0] < arr[1] && arr[1] < arr[2] && arr[2] < arr[3]) {
+            System.out.println("Массив строго возрастающей последовательностью");
+        } else {
+            System.out.println("Массив НЕ строго возрастающей последовательностью");
+        }
+    }
+}
